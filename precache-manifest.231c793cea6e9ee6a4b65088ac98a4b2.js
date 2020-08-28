fd1a170292e8d9c357e223fc35437ee16816d619
4saltysalty <argusxian@gmail.com>
2020-08-28 17:48:50 +0800

    Updates

---
+++ precache-manifest.231c793cea6e9ee6a4b65088ac98a4b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02fd1013c245344e6abeaaa9f0f1fc66",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "127b830cda7f5b13e6b6",
+    "url": "/monsters-rolodex/static/css/main.53393c06.chunk.css"
+  },
+  {
+    "revision": "ce36ca03a633ea901a54",
+    "url": "/monsters-rolodex/static/js/2.debc5020.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.debc5020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "127b830cda7f5b13e6b6",
+    "url": "/monsters-rolodex/static/js/main.2bb59e84.chunk.js"
+  },
+  {
+    "revision": "94fa8f99448f1ae0cf32",
+    "url": "/monsters-rolodex/static/js/runtime-main.f635ba4d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
